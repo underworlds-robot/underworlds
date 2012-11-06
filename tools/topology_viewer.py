9d27b9c461ba8d8a8072a57314eafedb5d4b198e
Séverin Lemaignan <severin.lemaignan@laas.fr>
2012-11-06 11:35:59 +0100

    topology viewer: update the color of edges to reflect the 'heat;'of the link
    Recent activity -> Red edge ; no recent activity (>30sec) -> blue edge. Nice gradient inbetween

--- tools/topology_viewer.py
+++ tools/topology_viewer.py
@@ -27,6 +27,78 @@ COL_WORLDS_BORDER = "A66200"
 COL_CLIENTS = "00AB6F"
 COL_EDGES = "0E0874"
 
+def heat_to_rgb(v, maxvalue = 1.0, minvalue = 0.0):
+    min_visible_wavelength = 450.0
+    max_visible_wavelength = 700.0
+
+    wavelength = (v - minvalue) / (maxvalue-minvalue) * \
+                 (max_visible_wavelength - min_visible_wavelength) + \
+                 min_visible_wavelength
+
+    rgb = wavelength_to_rgb(wavelength)
+
+    return "%02x%02x%02x" % rgb
+
+def wavelength_to_rgb(wl):
+    """ Converts a visible wavelength (in nanometer) to the corresponding
+    R,B,G triple.
+
+    Based on: http://stackoverflow.com/questions/2374959/algorithm-to-convert-any-positive-integer-to-an-rgb-value
+    """
+    gamma = 0.80
+    intensity_max = 255
+
+    def adjust(color, factor):
+        if color == 0.0:
+            return 0 # Don't want 0^x = 1 for x <> 0
+        else:
+            return int(intensity_max * pow(color * factor, gamma))
+    
+    if wl >= 380 and wl < 440:
+        red = -(wl - 440) / (440 - 380)
+        green = 0.0
+        blue = 1.0
+    elif wl >= 440 and wl < 490:
+        red = 0.0
+        green = (wl - 440) / (490 - 440)
+        blue = 1.0
+    elif wl >= 490 and wl < 510:
+        red = 0.0
+        green = 1.0
+        blue = -(wl - 510) / (510 - 490)
+    elif wl >= 510 and wl < 580:
+        red = (wl - 510) / (580 - 510)
+        green = 1.0
+        blue = 0.0
+    elif wl >= 580 and wl < 645:
+        red = 1.0
+        green = -(wl - 645) / (645 - 580)
+        blue = 0.0
+    elif wl >= 645 and wl <= 780:
+        red = 1.0
+        green = 0.0
+        blue = 0.0
+    else:
+        red = 0.0
+        green = 0.0
+        blue = 0.0
+
+    # Let the intensity fall off near the vision limits
+    if wl >= 380 and wl < 420:
+        factor = 0.3 + 0.7 * (wl - 380) / (420 - 380)
+    elif wl >= 420 and wl < 700:
+        factor = 1.0
+    elif wl >= 700 and wl < 780:
+        factor = 0.3 + 0.7 * (780 - wl) / (780 - 700)
+    else:
+        factor = 0.0
+
+    r = adjust(red,   factor)
+    g = adjust(green, factor)
+    b = adjust(blue,  factor)
+
+    return (r,g,b)
+
 class UnderworldsDotWindow(xdot.DotWindow):
 
     ui = '''
@@ -84,6 +156,8 @@ class UnderworldsDotWindow(xdot.DotWindow):
 
                 last_activity = time.time() - timestamp
                 
+                heat = max(0.0, 1.0 - (last_activity / 30))
+                
                 label = "%s\\n(last activity: " % type.lower()
                 if last_activity < 2:
                     label += "%d ms" % (last_activity * 1000)
@@ -101,7 +175,8 @@ class UnderworldsDotWindow(xdot.DotWindow):
                     edge = '"%s" <-> "%s"' % (w, c)
                 else:
                     edge = '"%s" -> "%s"' % (c, w)
-                dotcode += edge + ' [label="%s", color="#%s", fontsize=8];\n' % (label, COL_EDGES)
+                dotcode += edge + ' [label="%s", color="#%s", fontsize=8];\n' % (label, heat_to_rgb(heat))
+                #dotcode += edge + ' [label="%s", color="#%s", fontsize=8];\n' % (label, COL_EDGES)
 
         dotcode += "}\n"
 
