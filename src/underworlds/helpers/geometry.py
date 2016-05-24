underworlds-robot/underworlds

commit 95a399adf9e0a5f992b40f26b3024eded7a4abe7

Moved some common geometry-related routines to helpers/geometry.py

src/underworlds/helpers/geometry.py
@@ -1,0 +1,61 @@
+import numpy
+from numpy import linalg
+
+def transform(vector3, matrix4x4):
+    """ Apply a transformation matrix on a 3D vector.
+
+    :param vector3: a numpy array with 3 elements
+    :param matrix4x4: a numpy 4x4 matrix
+    """
+    return numpy.dot(matrix4x4, numpy.append(vector3, 1.))
+
+def get_scene_bounding_box(scene):
+    """
+    Returns the axis-aligned bounding box (AABB) of a whole scene,
+    or None if the scene is empty.
+    """
+    bb_min = [1e10, 1e10, 1e10] # x,y,z
+    bb_max = [-1e10, -1e10, -1e10] # x,y,z
+
+    if not scene.rootnode.children:
+        logger.warning("rootnode has no children! The scene is probably empty.")
+        return None, None
+
+    return _merge_with_bounding_box_for_node(scene, scene.rootnode, bb_min, bb_max, linalg.inv(scene.rootnode.transformation))
+
+def _merge_with_bounding_box_for_node(scene, node, bb_min, bb_max, transformation):
+
+    transformation = numpy.dot(transformation, node.transformation)
+    if node.type == MESH:
+        for v in node.aabb:
+            v = transform(v, transformation)
+            bb_min[0] = min(bb_min[0], v[0])
+            bb_min[1] = min(bb_min[1], v[1])
+            bb_min[2] = min(bb_min[2], v[2])
+            bb_max[0] = max(bb_max[0], v[0])
+            bb_max[1] = max(bb_max[1], v[1])
+            bb_max[2] = max(bb_max[2], v[2])
+
+    for child in node.children:
+        bb_min, bb_max = get_bounding_box_for_node(scene, scene.nodes[child], bb_min, bb_max, transformation)
+
+    return bb_min, bb_max
+
+def _get_parent_chain(scene, node, parents):
+    parents.append(node.parent)
+
+    if node.parent == scene.rootnode:
+        return parents
+
+    return _get_parent_chain(scene, node.parent, parents)
+
+def transformed_aabb(scene, node):
+    """ TODO: this computation is incorrect! To compute a transformed AABB, we need to
+    re-compute the AABB from the transformed *vertices* (or maybe from the transformed bounding box?)
+    """
+    parents = reversed(_get_parent_chain(scene, node, []))
+
+    global_transformation = reduce(numpy.dot, [p.transformation for p in parents])
+    
+    return transform(node.aabb[0], global_transformation), \
+           transform(node.aabb[1], global_transformation)

bin/uwds-view
@@ -43,6 +43,7 @@ from numpy import linalg
 import underworlds
 from underworlds import situations
 from underworlds.types import *
+from underworlds.helpers.geometry import transform, get_scene_bounding_box
 
 
 #rendering mode
@@ -109,47 +110,11 @@ BASIC_FRAGMENT_SHADER="""
 #version 120
 
 varying vec4 v_color;
+
 void main() {
     gl_FragColor = v_color;
 }
 """
-
-def transform(vector3, matrix4x4):
-    """ Apply a transformation matrix on a 3D vector.
-
-    :param vector3: a numpy array with 3 elements
-    :param matrix4x4: a numpy 4x4 matrix
-    """
-    return numpy.dot(matrix4x4, numpy.append(vector3, 1.))
-
-def get_bounding_box(scene):
-    nodes = scene.nodes
-    bb_min = [1e10, 1e10, 1e10] # x,y,z
-    bb_max = [-1e10, -1e10, -1e10] # x,y,z
-
-    if not scene.rootnode.children:
-        logger.warning("rootnode has no children! The scene is probably empty.")
-        return None, None
-
-    return get_bounding_box_for_node(nodes, scene.rootnode, bb_min, bb_max, linalg.inv(scene.rootnode.transformation))
-
-def get_bounding_box_for_node(nodes, node, bb_min, bb_max, transformation):
-
-    transformation = numpy.dot(transformation, node.transformation)
-    if node.type == MESH:
-        for v in node.aabb:
-            v = transform(v, transformation)
-            bb_min[0] = min(bb_min[0], v[0])
-            bb_min[1] = min(bb_min[1], v[1])
-            bb_min[2] = min(bb_min[2], v[2])
-            bb_max[0] = max(bb_max[0], v[0])
-            bb_max[1] = max(bb_max[1], v[1])
-            bb_max[2] = max(bb_max[2], v[2])
-
-    for child in node.children:
-        bb_min, bb_max = get_bounding_box_for_node(nodes, nodes[child], bb_min, bb_max, transformation)
-
-    return bb_min, bb_max
 
 class DefaultCamera:
     def __init__(self, w, h, fov):
@@ -404,12 +369,14 @@ class Underworlds3DViewer:
 
         #log some statistics
         logger.info("  -> %d nodes" % len(nodes))
-        self.bb_min, self.bb_max = get_bounding_box(scene)
 
-        if not self.bb_min:
+        scene_aabb = get_scene_bounding_box(scene)
+
+        if not scene_aabb:
             logger.warning("Nothing to display. Leaving now.")
             sys.exit(0)
 
+        self.bb_min, self.bb_max = scene_aabb
         logger.info("  -> scene bounding box:" + str(self.bb_min) + " - " + str(self.bb_max))
 
         self.scene_center = [(a + b) / 2. for a, b in zip(self.bb_min, self.bb_max)]

clients/visibility_monitor.py
@@ -27,14 +27,7 @@ logging.basicConfig(level=logging.INFO)
 
 import underworlds
 from underworlds.types import *
-
-def transform(vector3, matrix4x4):
-    """ Apply a transformation matrix on a 3D vector.
-
-    :param vector3: a numpy array with 3 elements
-    :param matrix4x4: a numpy 4x4 matrix
-    """
-    return numpy.dot(matrix4x4, numpy.append(vector3, 1.))
+from underworlds.helpers.geometry import transform
 
 class VisibilityMonitor:
 
@@ -250,10 +243,6 @@ class VisibilityMonitor:
         glUseProgram( 0 )
 
     def check_visibility(self, debug = False):
-        """
-        Attention: The performances of this method relies heavily on the size of the display!
-        """
-
         for c in self.cameras:
                 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)

commit ad3c3213f69f7a69106cf438e39a11bc1b369665

Fix bugs introduced by 95a399adf9e0a5f9 and a036844024e

src/underworlds/helpers/geometry.py
@@ -1,6 +1,10 @@
 import numpy
 from numpy import linalg
 
+import logging; logger = logging.getLogger("underworlds.helpers.geometry")
+
+from ..types import MESH
+
 def transform(vector3, matrix4x4):
     """ Apply a transformation matrix on a 3D vector.
 
@@ -21,10 +25,10 @@ def get_scene_bounding_box(scene):
         logger.warning("rootnode has no children! The scene is probably empty.")
         return None, None
 
-    return _merge_with_bounding_box_for_node(scene, scene.rootnode, bb_min, bb_max, linalg.inv(scene.rootnode.transformation))
+    return get_bounding_box_for_node(scene.nodes, scene.rootnode, bb_min, bb_max, linalg.inv(scene.rootnode.transformation))
 
-def _merge_with_bounding_box_for_node(scene, node, bb_min, bb_max, transformation):
-
+def get_bounding_box_for_node(nodes, node, bb_min, bb_max, transformation):
+    
     transformation = numpy.dot(transformation, node.transformation)
     if node.type == MESH:
         for v in node.aabb:
@@ -37,7 +41,7 @@ def _merge_with_bounding_box_for_node(scene, node, bb_min, bb_max, transformatio
             bb_max[2] = max(bb_max[2], v[2])
 
     for child in node.children:
-        bb_min, bb_max = get_bounding_box_for_node(scene, scene.nodes[child], bb_min, bb_max, transformation)
+        bb_min, bb_max = get_bounding_box_for_node(nodes, nodes[child], bb_min, bb_max, transformation)
 
     return bb_min, bb_max
 

src/underworlds/tools/loader.py
@@ -51,7 +51,8 @@ class ModelLoader:
         logger.debug("Parsing node " + str(assimp_node))
         underworlds_node.name = assimp_node.name
 
-        underworlds_node.parent = self.node_map[assimp_node.parent.name][1].id
+        if assimp_node is not assimp_model.rootnode:
+            underworlds_node.parent = self.node_map[assimp_node.parent.name][1].id
 
         for c in assimp_node.children:
             underworlds_node.children.append(self.node_map[c.name][1].id)

src/underworlds/types.py
@@ -73,6 +73,8 @@ class Node():
 
 
 class Scene():
+    """An Underworlds scene
+    """
 
     def __init__(self):
 