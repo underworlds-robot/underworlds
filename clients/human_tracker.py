d2e7176648fac878c588f09da5489bea22d9147b
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2016-06-08 16:35:26 +0100

    [human tracker] Publish all the frame of the upper body
    While here, sorted out transformation issues. This should be all good now

--- clients/human_tracker.py
+++ clients/human_tracker.py
@@ -13,6 +13,46 @@ import underworlds
 from underworlds.helpers import transformations
 from underworlds.types import Node, ENTITY
 
+# Attention!! The labels for the right and left sides are inverted so that when
+# the user move the left hand, it also moves the left hand of the 'avatar'
+# See page 13 of http://www.openni.ru/wp-content/uploads/2013/02/NITE-Algorithms.pdf
+# for the details.
+joints = {"head":          nite2.JointType.NITE_JOINT_HEAD,
+          "neck":          nite2.JointType.NITE_JOINT_NECK,
+          "torso":         nite2.JointType.NITE_JOINT_TORSO,
+          "right_shoulder":nite2.JointType.NITE_JOINT_LEFT_SHOULDER,
+          "left_shoulder": nite2.JointType.NITE_JOINT_RIGHT_SHOULDER,
+          "right_elbow":   nite2.JointType.NITE_JOINT_LEFT_ELBOW,
+          "left_elbow":    nite2.JointType.NITE_JOINT_RIGHT_ELBOW,
+          "right_hand":    nite2.JointType.NITE_JOINT_LEFT_HAND,
+          "left_hand":     nite2.JointType.NITE_JOINT_RIGHT_HAND
+          }
+
+
+def transformation_matrix(user, joint_code):
+    joint = user.skeleton.joints[joint_code]
+
+    #confidence = head.positionConfidence
+
+    translation = (joint.position.x/1000.,
+                   joint.position.y/1000.,
+                   joint.position.z/1000.)
+
+
+    translation_mat = transformations.translation_matrix(translation)
+
+    orientation = (joint.orientation.x, 
+                    joint.orientation.y,
+                    joint.orientation.z,
+                    joint.orientation.w)
+
+    rotation_mat = transformations.quaternion_matrix(orientation)
+
+    # mirror back the skeleton along the X axis, as the freenect driver mirrors the
+    # RGBD image by default
+    mirror_mat = transformations.reflection_matrix((0,0,0),(1,0,0))
+
+    return numpy.dot(mirror_mat, numpy.dot(translation_mat, rotation_mat))
 
 def process_frame(frame):
 
@@ -22,25 +62,8 @@ def process_frame(frame):
                 logger.info("New human detected! Calibrating...")
                 userTracker.start_skeleton_tracking(user.id)
             elif user.skeleton.state == nite2.SkeletonState.NITE_SKELETON_TRACKED:
-                head = user.skeleton.joints[nite2.JointType.NITE_JOINT_HEAD]
 
-                #confidence = head.positionConfidence
-
-                translation = (head.position.x/1000.,
-                               head.position.y/1000.,
-                               head.position.z/1000.)
-
-
-                translation_mat = transformations.translation_matrix(translation)
-
-                orientation = (head.orientation.x, 
-                            head.orientation.y,
-                            head.orientation.z,
-                            head.orientation.w)
-
-                rotation_mat = transformations.quaternion_matrix(orientation)
-
-                return numpy.dot(translation_mat, rotation_mat)
+                return {name: transformation_matrix(user, joint_code) for name, joint_code in joints.items()}
 
 
 if __name__ == "__main__":
@@ -86,15 +109,24 @@ if __name__ == "__main__":
         camera = Node("kinect", ENTITY)
 
         translation_cam=transformations.translation_matrix((1,0,0.5))
-        rotation_cam=transformations.euler_matrix(math.pi/2,0,0)
+
+        # According to http://www.openni.ru/wp-content/uploads/2013/02/NITE-Algorithms.pdf
+        # the sensor is oriented as follow:
+        # " +X points to the right of the, +Y points up, and +Z
+        # points in the direction of increasing depth."
+        rotation_cam=transformations.euler_matrix(math.pi/2,0,math.pi/2)
 
         camera.transformation = numpy.dot(translation_cam, rotation_cam)
         camera.parent = world.scene.rootnode.id
         nodes.append(camera)
 
-        head = Node("head", ENTITY)
-        head.parent = camera.id
-        nodes.append(head)
+
+        # Create and append nodes for every body part
+        human_nodes = {name: Node(name, ENTITY) for name in joints.keys()}
+
+        for _, node in human_nodes.items():
+            node.parent = camera.id
+            nodes.append(node)
 
         try:
             while True:
@@ -102,17 +134,21 @@ if __name__ == "__main__":
                 frame = userTracker.read_frame()
                 #logger.info("%s humans detected" % len(frame.users))
 
-                transformation = process_frame(frame)
+                humanparts = process_frame(frame)
 
-                if transformation is not None:
-                    head.transformation = transformation
-                    nodes.update(head)
+                if humanparts is not None:
+                    for name, node in human_nodes.items():
+                        node.transformation = humanparts[name]
+                        nodes.update(node)
 
 
         except KeyboardInterrupt:
             logger.info("Quitting...")
 
-        nodes.remove(head)
+        ## Clean up
+        for _, node in human_nodes.items():
+            nodes.remove(node)
+
         nodes.remove(camera)
 
     nite2.unload()
