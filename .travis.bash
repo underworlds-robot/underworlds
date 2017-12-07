f1044d538339032a696972cde192a212c89de92a
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2017-12-07 15:35:18 +0000

    [travis] Trying to fix travis compilation

--- .travis.bash
+++ .travis.bash
@@ -12,22 +12,36 @@ mkdir -p ${UWDS_PREFIX}
 echo "Installing pyassimp"
 
 cd ${HOME}
+
 if [ ! -d "assimp" ]; then
     git clone --depth=1 https://github.com/assimp/assimp.git
-fi
-cd assimp
-mkdir -p build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF ..
-make -j4
-sudo make install
 
-cd ../port/PyAssimp
+    echo "Content of HOME"
+
+    ls
+
+    cd ${HOME}/assimp
+    echo "Content of `pwd`:"
+    ls
+
+    mkdir -p ${HOME}/assimp/build
+    cd ${HOME}/assimp/build
+    echo "Current path: `pwd`"
+
+
+    cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF ..
+    make -j4
+    sudo make install
+
+fi
+
+cd ${HOME}/assimp/port/PyAssimp
 
 # workaround for bug introduced in assimp's 33bd5cfcfb0f27794a333273b20b60a7a550d184
 mkdir -p ../../lib
 
 python setup.py install --prefix=${UWDS_PREFIX}
+
 
 ################################################################################
 ################################################################################
