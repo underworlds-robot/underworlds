6ae5a1efe5a04c590decceb501e0440ba18b484b
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2018-01-24 15:38:59 +0000

    [test]performances.py: make it possible to test with multiple changes

--- testing/performances.py
+++ testing/performances.py
@@ -26,9 +26,8 @@ def passthrough(world1, world2, signaling_pipe):
     propagate these changes to world world2.
     """
 
-    client_id = str(uuid.uuid4())
-    print("Hello, I'm client %s!" % client_id)
-    with underworlds.Context("client_%s" % client_id) as ctx:
+    name = "passthrough_filter_%s_to_%s" % (world1, world2)
+    with underworlds.Context(name) as ctx:
 
         world1 = ctx.worlds[world1]
         world2 = ctx.worlds[world2]
@@ -36,11 +35,14 @@ def passthrough(world1, world2, signaling_pipe):
         try:
             print("Waiting for changes...")
             while not signaling_pipe.poll():
+                #print("%f -- %s waiting" % (time.time(), name))
                 change = world1.scene.waitforchanges(0.5)
+                #print("%f -- %s Done waiting (last change: %s)" % (time.time(), name, str(change)))
                 if change is not None:
                     id, op = change
-                    print("%f -- Propagating from %s to %s" % (time.time(), world1, world2))
+                    #print("%f -- propagating from %s to %s" % (time.time(), name, world1, world2))
                     world2.scene.update_and_propagate(world1.scene.nodes[id])
+                    change = None
         except Exception as e:
             import traceback
             traceback.print_exc()
@@ -48,20 +50,25 @@ def passthrough(world1, world2, signaling_pipe):
     print("Passthrough stopped")
 
 
-def wait_for_changes(world):
+def wait_for_changes(world, nb_changes):
 
     print("Waiting for changes in world %s" % world)
 
     starttime = time.time()
 
-    change = world.scene.waitforchanges(5)
+    change = None
+
+    for i in range(nb_changes):
+        change = world.scene.waitforchanges(3)
+
     profileonce("waitforchanges triggered")
+
     return change, time.time()-starttime
 
 
 
     
-def test_propagation_time(nb_worlds):
+def test_propagation_time(nb_worlds, nb_changes):
 
     executor = ThreadPoolExecutor(max_workers=nb_worlds)
     pool = Pool(nb_worlds)
@@ -81,31 +88,31 @@ def test_propagation_time(nb_worlds):
     entry_world = ctx.worlds["world0"]
     exit_world = ctx.worlds["world%d" % (nb_worlds-1)]
 
-    future = executor.submit(wait_for_changes, exit_world)
+    future = executor.submit(wait_for_changes, exit_world, nb_changes)
 
-    n = Node()
-    n.name = "test"
-
-    print("\n\n\nPropagating a change from world %s..." % entry_world)
+    print("\n\n\nPropagating %d change(s) from world %s..." % (nb_changes, entry_world))
     profileonce("start test with %d worlds" % nb_worlds)
 
-    starttime=time.time()
 
-    entry_world.scene.append_and_propagate(n)
+    for i in range(nb_changes):
+        n = Node()
+        n.name = "node_%d" % i
+        entry_world.scene.append_and_propagate(n)
+        time.sleep(0.01)
 
-    change, duration = future.result()
+    seen, duration = future.result()
 
     profileonce("end test")
 
-    print("It took %s to be notified of the change in world %s" % (ms(duration), exit_world))
 
-    if change is None:
-        raise RuntimeError("The change has not been seen!")
-
-    print("Total time: %.2f" % ((time.time() - starttime) * 1000))
+    if seen is None:
+        logger.error("The changes have not been seen!")
+        duration = 0
+    else:
+        print("It took %s to be notified of the %d change(s) in world %s" % (ms(duration), nb_changes, exit_world))
 
     executor.shutdown(wait=True)
-    #pool.terminate()
+
     for p in pipes:
         p.send(True)
     pool.close()
@@ -123,6 +130,7 @@ if __name__ == '__main__':
     parser.add_argument("-dd", "--fulldebug", help="debug mode (verbose)", action="store_true")
     parser.add_argument("-i", "--incremental", action="store_true", help="test for every nb of worlds, from 2 to maxworlds")
     parser.add_argument("-r", "--repeat", default=1, type=int, nargs="?", help="how many times the test should be repeated (default: no repeat)")
+    parser.add_argument("-c", "--changes", default=1, type=int, nargs="?", help="how many changes should be propagated (default: one)")
     args = parser.parse_args()
 
     if args.debug or args.fulldebug:
@@ -142,9 +150,10 @@ if __name__ == '__main__':
             print("\n\n\n-- %d worlds --\n" % nb)
 
             server = underworlds.server.start()
-
-            durations.setdefault(nb,[]).append(test_propagation_time(nb))
+            durations.setdefault(nb,[]).append(test_propagation_time(nb, args.changes))
             server.stop(0).wait()
+
+
         time.sleep(0.5)
 
         for nb,times in durations.items():
